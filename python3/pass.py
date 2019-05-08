3d1277495affb509d7fab100197bbaedcaec1f01
Hugh Pearse <hughpear@ie.ibm.com>
2019-05-08 18:22:23 +0100

    updated code for image param parsing

--- python3/pass.py
+++ python3/pass.py
@@ -6,29 +6,45 @@ import hmac
 import hashlib
 import urllib.parse as urlparse
 import base64
+import sys
 
-numberOfDigitsRequiredInOTP = 6
-lifetimeOfOTPInSeconds = 30
-print("OTP length:", numberOfDigitsRequiredInOTP)
-print("OTP lifetime:", lifetimeOfOTPInSeconds)
+if len(sys.argv) < 2:
+    print("Usage: python3 " + sys.argv[0] + " qr-code.png")
+    sys.exit(-1)
 
-qr_code = fastzbarlight.scan_codes('qrcode', Image.open("./qr.png"))
+qr_code = fastzbarlight.scan_codes('qrcode', Image.open(sys.argv[1]))
 qr_code = str(qr_code[0].decode())
 print("QR code:", qr_code)
 
+secret = None
+digits = 6
+period = 30
+algo = hashlib.sha1
 parsed = urlparse.urlparse(qr_code)
-secret = urlparse.parse_qs(parsed.query)["secret"][0]
+qs = urlparse.parse_qs(parsed.query)
+for k,v in qs.items():
+    if k == "secret":
+        secret = v[0]
+    if k == "digits":
+        digits = int(v[0])
+    if k == "period":
+        period = int(v[0])
+    if k == "algorithm":
+        algo = getattr(hashlib, v[0].lower())
 print("secret:", secret)
+print("OTP length:", digits)
+print("OTP lifetime:", period)
+
 secret = base64.b32decode(secret)
 
 currentUnixTime = int(time.time())
 print("Unix time:", currentUnixTime)
 
-counter = currentUnixTime // lifetimeOfOTPInSeconds
+counter = currentUnixTime // period
 print("Counter:", counter)
 counter = counter.to_bytes(8, byteorder = 'big')
 
-hash = hmac.new(secret, counter, hashlib.sha1)
+hash = hmac.new(secret, counter, algo)
 digest = hash.digest()
 print("HMAC Digest", hash.hexdigest())
 
@@ -36,7 +52,7 @@ offset = digest[19] & 0xf # last nibble operations
 print("offset:", offset)
 truncatedHash = (digest[offset] & 0x7f) << 24 | (digest[offset+1] & 0xff) << 16 | (digest[offset+2] & 0xff) << 8 | (digest[offset+3] & 0xff)
 print("truncatedHash:", hex(truncatedHash))
-finalOTP = (truncatedHash % (10 ** numberOfDigitsRequiredInOTP))
+finalOTP = (truncatedHash % (10 ** digits))
 print("finalOTP:", finalOTP)
 
 
